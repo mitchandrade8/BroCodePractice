0e5222376562ee401a13cffc687735aaab58c321
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-18 11:16:06 -0700

    --stuck trying to compile still

--- SetupStuff/helloworld.cpp
+++ SetupStuff/helloworld.cpp
@@ -5,13 +5,15 @@
 
 using namespace std;
 
-int main()
-{
+int main() {
+
     vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+
 
     for (const string& word : msg)
     {
         cout << word << " ";
     }
+    
     cout << endl;
 }
