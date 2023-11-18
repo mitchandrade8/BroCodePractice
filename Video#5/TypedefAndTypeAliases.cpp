b0adc569962afc6b72bb241681f65cbdf3d4ad94
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-18 13:11:06 -0700

    --More project organization

--- Video#5/TypedefAndTypeAliases.cpp
+++ Video#5/TypedefAndTypeAliases.cpp
@@ -1,9 +1,23 @@
 
 #include <iostream>
+#include <vector>
+
+// typedef std::vector<std::pair<std::string, int>> pairlist_t ;
+
+typedef std::string text_t;
+typedef int number_t;
 
 int main() {
 
+    // typedef = reserved keyword used to create an additional name (alias) for another data type.
+    // New identifier for an existing type
+    // Helps with readability and reduces typos.
 
+    text_t firstName = "Bro";
+    number_t age = 21;
+
+    std::cout << firstName << '\n';
+    std::cout << age << '\n';
 
     return 0;
     
