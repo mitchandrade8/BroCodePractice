7937236215bae46aa42cc98250e18b2636f1fc9f
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-18 13:18:00 -0700

    --Start of arithmetic operators in Cpp

---
+++ Video#6/arithmeticOperators.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream>
+
+int main() {
+
+    // arithmetic operators = return the result of a specific arithmetic operation 
+    //                  ( + - * / )
+
+    
+
+
+    return 0;
+
+}
