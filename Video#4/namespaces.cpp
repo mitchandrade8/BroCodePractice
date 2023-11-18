c8735927df3d66aaf0262569a346e3f12b6b785f
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-18 11:50:14 -0700

    --details, namespace can change values

--- Video#4/namespaces.cpp
+++ Video#4/namespaces.cpp
@@ -6,7 +6,28 @@ int main() {
     // Namespace = provides a solution for preventing name conflicts in large projects. Each entity needs a unique name.
     // A namespace allows for identically named entities as long as the namespaces are different.
 
+    // Example #1:
+    int x = 0;
+
+    std::cout << x;
+
+    // Refering to a diferent namespace x
+    std::cout << first::x;
+
+    // ** Key Note: Must explicity tell the compiler which namespace we are using otherwise the default will be used.
 
     return 0;
+
+}
+
+namespace first {
+
+    int x = 1;
+
+}
+
+namespace second {
+
+    int x = 2;
 
 }
