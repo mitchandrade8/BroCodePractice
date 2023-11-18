8ea1e23a44fc9ac5c91962dcc87ae636479423c7
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-18 13:14:30 -0700

    --Finished typedef and type aliases

--- Video#5/TypedefAndTypeAliases.cpp
+++ Video#5/TypedefAndTypeAliases.cpp
@@ -4,14 +4,20 @@
 
 // typedef std::vector<std::pair<std::string, int>> pairlist_t ;
 
-typedef std::string text_t;
-typedef int number_t;
+// typedef std::string text_t;
+// typedef int number_t;
+
+using text_t = std::string;
+using number_t = int;
 
 int main() {
 
     // typedef = reserved keyword used to create an additional name (alias) for another data type.
     // New identifier for an existing type
     // Helps with readability and reduces typos.
+
+    // Use when there is a clear benefit
+    // Replaced with 'using' (work better with templates)
 
     text_t firstName = "Bro";
     number_t age = 21;
