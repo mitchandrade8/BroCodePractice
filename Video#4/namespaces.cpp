cbbe4b16682b247974f950f20986f4ae9448647a
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-18 11:37:24 -0700

    --Start of going over namespaces

---
+++ Video#4/namespaces.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+
+int main() {
+
+    // Namespace = provides a solution for preventing name conflicts in large projects. Each entity needs a unique name.
+    // A namespace allows for identically named entities as long as the namespaces are different.
+
+
+    return 0;
+
+}
