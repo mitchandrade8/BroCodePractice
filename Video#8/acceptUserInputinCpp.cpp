3ed7e0a94dae5a78b7e348235b7062a4950de4d3
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-19 14:53:11 -0700

    --Finished Accepting User Input in Cpp

--- Video#8/acceptUserInputinCpp.cpp
+++ Video#8/acceptUserInputinCpp.cpp
@@ -10,14 +10,26 @@ int main() {
     std::string name;
     int age;
 
-    std::cout << "What's your full name?: ";
-    std::getline(std::cin, name);
+    // MARK: - Second Example, how to handle Whitespaces
 
     std::cout << "What is your age?: ";
     std::cin >> age;
 
-    std::cout << "Hello " << name << " ";
+    std::cout << "What is your full name?: ";
+    std::getline(std::cin >> std::ws, name);
+
+    std::cout << "Hello " << name << '\n';
     std::cout << "You are " << age << " years old";
+
+    // MARK: - First Example
+    //std::cout << "What's your full name?: ";
+    //std::getline(std::cin, name);
+
+    //std::cout << "What is your age?: ";
+    //std::cin >> age;
+
+    //std::cout << "Hello " << name << '\n';
+    //std::cout << "You are " << age << " years old";
 
 
     return 0;
