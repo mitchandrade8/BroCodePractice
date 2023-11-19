896f8cde2bee4216dc853fd47e97f17f22ce56d4
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-19 14:44:13 -0700

    Cpp Functions for input with spaces

--- Video#8/acceptUserInputinCpp.cpp
+++ Video#8/acceptUserInputinCpp.cpp
@@ -10,13 +10,13 @@ int main() {
     std::string name;
     int age;
 
-    std::cout << "What's your name?: ";
-    std::cin >> name;
+    std::cout << "What's your full name?: ";
+    std::getline(std::cin, name);
 
     std::cout << "What is your age?: ";
     std::cin >> age;
 
-    std::cout << "Hello " << name;
+    std::cout << "Hello " << name << " ";
     std::cout << "You are " << age << " years old";
 
 
