5f153c928e222ff4d255931ee6a01ea357b7da9c
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-19 14:39:44 -0700

    --got it to run!!! no errors!!!

--- Video#8/acceptUserInputinCpp.cpp
+++ Video#8/acceptUserInputinCpp.cpp
@@ -8,15 +8,18 @@
 int main() {
 
     std::string name;
+    int age;
 
     std::cout << "What's your name?: ";
-
     std::cin >> name;
 
+    std::cout << "What is your age?: ";
+    std::cin >> age;
+
     std::cout << "Hello " << name;
+    std::cout << "You are " << age << " years old";
 
 
     return 0;
-
 
 }
