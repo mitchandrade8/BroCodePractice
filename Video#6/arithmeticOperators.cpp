00e34e68cc0a5714cc7687a1cbfa1e946d05643b
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-19 12:07:11 -0700

    --Continuing arithmetic operators Cpp

--- Video#6/arithmeticOperators.cpp
+++ Video#6/arithmeticOperators.cpp
@@ -6,8 +6,30 @@ int main() {
     // arithmetic operators = return the result of a specific arithmetic operation 
     //                  ( + - * / )
 
-    
+    int students = 20;
 
+        /// students = students + 1;
+
+    // Shorthand syntax:
+        /// students += 1;
+
+    // increment by 1 syntax:
+    students ++;
+
+    std::cout << students;
+
+    // MARK: - Subtraction
+
+        /// students = students - 1 ;
+
+    // Shorthard syntax for subtraction:
+
+        /// students -= 1;
+
+    // Decrement by 1 syntax: 
+
+    students --;
+    
 
     return 0;
 
