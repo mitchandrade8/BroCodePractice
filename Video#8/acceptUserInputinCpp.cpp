637add6b443646a91a7c1f55759aab8ee3b40ae0
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-19 14:35:04 -0700

    --made some progress with running and errors

--- Video#8/acceptUserInputinCpp.cpp
+++ Video#8/acceptUserInputinCpp.cpp
@@ -5,11 +5,15 @@
     // cout << (insertion operator)
     // cin >> (extraction operator)
 
-int main () {
+int main() {
 
     std::string name;
 
-    
+    std::cout << "What's your name?: ";
+
+    std::cin >> name;
+
+    std::cout << "Hello " << name;
 
 
     return 0;
