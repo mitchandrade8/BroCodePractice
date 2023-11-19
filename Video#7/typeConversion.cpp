9cf748943077366624f1a570960ef04ca64624a8
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-19 14:23:09 -0700

    --Finished Type Conversion in Cpp

--- Video#7/typeConversion.cpp
+++ Video#7/typeConversion.cpp
@@ -3,8 +3,33 @@
 
 int main() {
 
+    // MARK: - Key Notes
+        // Type Conversion = conversion of a value of one data type to another
+                // Implicit = automatic
+                // Explicit = Precede value with new data type (int)
     
+    // Implicit cast Example:
+
+    /// double x = (int) 3.14;
+
+    /// std::cout << x;
+
+    // Another Example:
+
+    /// std::cout << 100;
+
+
+    // MARK: - Another Example
+    
+    int correct = 8;
+    int questions = 10;
+
+    double score = correct/(double)questions * 100;
+
+    std::cout << score << "%";
+
 
     return 0;
+
 
 }
