ab8041d257576e1e72cf5322d4820933f57500ee
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-19 18:44:21 -0700

    --Pow function in Cpp

--- Video#9/usefulMathFunctions.cpp
+++ Video#9/usefulMathFunctions.cpp
@@ -1,11 +1,21 @@
 
 #include <iostream>
+#include <cmath>
 
 int main() {
 
+    double x = 3;
+    double y = 4;
+    double z;
 
+    z = std::max(x, y);
 
+    // std::cout << z;
+
+    z = pow(2, 4);
+
+    std::cout << z;
 
     return 0;
-    
+
 }
