5b0a8db2de06de10c96b59bf4d5109cc29ab3762
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-19 18:55:26 -0700

    --breaking down the steps of Hypo-calc

--- Video#10/HypotenuseCalculatorPractice.cpp
+++ Video#10/HypotenuseCalculatorPractice.cpp
@@ -1,9 +1,22 @@
 
 #include <iostream>
+#include <cmath>
 
 int main() {
 
+    double a;
+    double b;
+    double c;
 
+    // Getting user input for calculation values
+    std::cout << "Enter side A: ";
+    std::cin >> a;
+
+    std::cout << "Enter side B: ";
+    std::cin >> b;
+
+
+    // Function to calculate the value of side 'c':
 
 
     return 0;
