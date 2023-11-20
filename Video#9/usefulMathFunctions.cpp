3eaab0e86360513acfd6cb75e136fb430f965206
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-19 18:50:29 -0700

    --Finished Useful Math Related Functions in Cpp

--- Video#9/usefulMathFunctions.cpp
+++ Video#9/usefulMathFunctions.cpp
@@ -8,11 +8,14 @@ int main() {
     double y = 4;
     double z;
 
-    z = std::max(x, y);
-
-    // std::cout << z;
-
-    z = pow(2, 4);
+    // z = std::max(x, y);
+    // z = std::min(x, y);
+    // z = pow(2, 4);
+    // z = sqrt(9);
+    // z = abs(-3);
+    // z = round(x);
+    // z = ceil(x);
+    // z = floor(x);
 
     std::cout << z;
 
