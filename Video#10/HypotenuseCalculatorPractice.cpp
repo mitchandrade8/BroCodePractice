c686cdaacfca08c4cd5048553b73d8b6b089e592
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-19 19:06:42 -0700

    -- Finished Hypotenuse calculator in Cpp

--- Video#10/HypotenuseCalculatorPractice.cpp
+++ Video#10/HypotenuseCalculatorPractice.cpp
@@ -15,10 +15,16 @@ int main() {
     std::cout << "Enter side B: ";
     std::cin >> b;
 
+    // Refactored Version:
+    c = sqrt(pow(a, 2) + pow(b, 2));
 
-    // Function to calculate the value of side 'c':
+    // First Version:
+    // a = pow(a, 2);
+    // b = pow(b, 2);
+    // c = sqrt(a + b);
 
-
+    std::cout << "side C: " << c;
+    
     return 0;
 
 }
